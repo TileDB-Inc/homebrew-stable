9528e0ec00f120441399ee1180351668ae7b5703
Jake Bolewski <jakebolewski@gmail.com>
2017-12-07 12:25:06 -0700

    add s3 build support

--- tiledb.rb
+++ tiledb.rb
@@ -8,8 +8,9 @@ class Tiledb < Formula
     head "https://github.com/TileDB-Inc/TileDB.git", :branch => "dev"
 
     option "with-debug", "Enables building with debug information"
-    option "with-hdfs",  "Enables building with HDFS integration"
     option "with-verbose", "Enables building with verbose status messages"
+    option "with-hdfs",  "Enables building with HDFS integration"
+    option "with-s3",    "Enables building with S3 object store integration"
 
     depends_on "cmake" => :build
     depends_on "lzlib"
@@ -17,6 +18,10 @@ class Tiledb < Formula
     depends_on "bzip2"
     depends_on "zstd"
     depends_on "blosc"
+
+    if build.with? "s3"
+        depends_on "aws-s3-sdk"
+    end
 
     def install
 	# Build and install TileDB
@@ -26,8 +31,10 @@ class Tiledb < Formula
 	      --prefix=#{prefix}
             ]
 	    args << "--enable-debug" if build.with? "debug"
-	    args << "--enable-hdfs" if build.with? "hdfs"
 	    args << "--enable-verbose" if build.with? "verbose"
+	    args << "--enable-hdfs" if build.with? "hdfs"
+	    args << "--enable-s3" if build.with? "s3"
+
             system "../bootstrap", *args
 	    system "make"
 	    system "make", "install"
