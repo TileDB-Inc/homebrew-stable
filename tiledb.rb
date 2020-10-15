ac3af72297328d0d204a2e1accacaa1ee93c127b
Seth Shelnutt <seth@tiledb.io>
2020-10-15 17:27:26 -0400

    Update to TileDB 2.1.1
    Enable GCS, Azure and Serialization
    
    Also update travis to remove double building and testing

--- tiledb.rb
+++ tiledb.rb
@@ -1,51 +1,53 @@
 class Tiledb < Formula
     desc "Storage management library for sparse and dense array data"
     homepage "http://tiledb.com"
-    url "https://github.com/TileDB-Inc/TileDB/archive/2.0.1.tar.gz"
-    sha256 "7260f8a7590d2fe8fd69d5008bf1514cd75d52425ae5a00a769b5c8d48aa18ba"
-    version "2.0.1"
+    url "https://github.com/TileDB-Inc/TileDB/archive/2.1.1.tar.gz"
+    sha256 "0df697ce9ac5858c9abc3d645f0fd96903b6512b33cf09ed4fc74d0cd4d78eec"
+    version "2.1.1"
 
     head "https://github.com/TileDB-Inc/TileDB.git", :branch => "dev"
 
     depends_on "cmake" => :build
-    depends_on "tbb"
     depends_on "lzlib"
     depends_on "lz4"
     depends_on "bzip2"
     depends_on "zstd"
 
     def install
-	# Build and install TileDB
-	mkdir "build"
-	cd "build" do
-	    args = %W[
-	      --prefix=#{prefix}
+        # Build and install TileDB
+        mkdir "build"
+        cd "build" do
+            args = %W[
+              --prefix=#{prefix}
               --enable-s3
-	      --enable-hdfs
-	      --disable-tests
+              --enable-azure
+              --enable-gcs
+              --enable-hdfs
+              --enable-serialization
+              --disable-tests
             ]
 
-	    system "../bootstrap", *args
+            system "../bootstrap", *args
 
-	    system "make"
-	    system "make", "-C", "tiledb"
-	    system "make", "-C", "tiledb", "install"
-	end
+            system "make", "-j4"
+            system "make", "-j4", "-C", "tiledb"
+            system "make", "-j4", "-C", "tiledb", "install"
+        end
     end
 
     test do
         (testpath/"test_tiledb.cpp").write <<~EOS
             #include "tiledb/tiledb.h"
-	    #include "assert.h"
-	    int main() {
-	        int major = 0;
-		int minor = 0;
-		int patch = 0;
-		tiledb_version(&major,&minor,&patch);
-		return 0;
-	    }
-	EOS
-	system ENV.cc, "test_tiledb.cpp", "-L#{lib}", "-ltiledb", "-o", "test"
-	system "./test"
+            #include "assert.h"
+            int main() {
+                int major = 0;
+                int minor = 0;
+                int patch = 0;
+                tiledb_version(&major,&minor,&patch);
+                return 0;
+            }
+        EOS
+        system ENV.cc, "test_tiledb.cpp", "-L#{lib}", "-ltiledb", "-o", "test"
+        system "./test"
     end
 end
