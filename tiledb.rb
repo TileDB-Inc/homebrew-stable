417b833f9c83d6a4c343163957aedaf48bb8fbb2
Isaiah Norton <isaiah.norton@gmail.com>
2022-06-23 08:02:44 -0400

    Run 'brew style --fix tiledb.rb' to try to fix install warning (did not)

--- tiledb.rb
+++ tiledb.rb
@@ -1,28 +1,32 @@
+# typed: false
+# frozen_string_literal: true
+
+# Storage management library for sparse and dense array data | github.com/TileDB-Inc
 class Tiledb < Formula
-    desc "Storage management library for sparse and dense array data"
-    homepage "http://tiledb.com"
-    url "https://github.com/TileDB-Inc/TileDB/releases/download/2.10.0/tiledb-macos-x86_64-2.10.0-0b54e51.tar.gz"
-    sha256 "57dce8fdc8e0fa1bf3b8e48366e529ddbf4bd948459d0893d068a680f1ab8d3f"
-    version "2.10.0"
+  desc "Storage management library for sparse and dense array data"
+  homepage "http://tiledb.com"
+  url "https://github.com/TileDB-Inc/TileDB/releases/download/2.10.0/tiledb-macos-x86_64-2.10.0-0b54e51.tar.gz"
+  version "2.10.0"
+  sha256 "57dce8fdc8e0fa1bf3b8e48366e529ddbf4bd948459d0893d068a680f1ab8d3f"
 
-    def install
-        # Build and install TileDB
-        prefix.install Dir["*"]
-    end
+  def install
+    # Build and install TileDB
+    prefix.install Dir["*"]
+  end
 
-    test do
-        (testpath/"test_tiledb.cpp").write <<~EOS
-            #include "tiledb/tiledb.h"
-            #include "assert.h"
-            int main() {
-                int major = 0;
-                int minor = 0;
-                int patch = 0;
-                tiledb_version(&major,&minor,&patch);
-                return 0;
-            }
-        EOS
-        system ENV.cc, "test_tiledb.cpp", "-L#{lib}", "-ltiledb", "-o", "test"
-        system "./test"
-    end
+  test do
+    (testpath/"test_tiledb.cpp").write <<~EOS
+      #include "tiledb/tiledb.h"
+      #include "assert.h"
+      int main() {
+          int major = 0;
+          int minor = 0;
+          int patch = 0;
+          tiledb_version(&major,&minor,&patch);
+          return 0;
+      }
+    EOS
+    system ENV.cc, "test_tiledb.cpp", "-L#{lib}", "-ltiledb", "-o", "test"
+    system "./test"
+  end
 end
