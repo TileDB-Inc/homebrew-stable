a6dcb5245753e06b2115f032f04fa920fd404682
Isaiah Norton <isaiah.norton@gmail.com>
2022-10-11 21:55:49 -0400

    Update to libtiledb 2.11.3

--- tiledb.rb
+++ tiledb.rb
@@ -1,23 +1,23 @@
 class Tiledb < Formula
   desc "Storage management library for sparse and dense array data"
   homepage "https://tiledb.com"
-  url "https://github.com/TileDB-Inc/TileDB/archive/refs/tags/2.11.0.tar.gz"
-  sha256 "98f1362a1394e6302c35ea388e480ae591031f49d5ee72087ac346191b239958"
+  url "https://github.com/TileDB-Inc/TileDB/archive/refs/tags/2.11.3.tar.gz"
+  sha256 "30375e953ca14584008cde4d722d0b084e862304a51282bc3ce5688e122af82f"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/TileDB-Inc/TileDB/releases/download/2.11.0/tiledb-macos-arm64-2.11.0-34e5dbc.tar.gz"
-    sha256 "fe7266839a39dc55560a7b0560d0ab286e67407b2549ce373adf6bd31cf6f6c3"
+    url "https://github.com/TileDB-Inc/TileDB/releases/download/2.11.3/tiledb-macos-arm64-2.11.3-a55a910.tar.gz"
+    sha256 "aa665070e16abcad30bb34069064e7795bdda7df3bfe8087500f7295be083969"
   end
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/TileDB-Inc/TileDB/releases/download/2.11.0/tiledb-macos-x86_64-2.11.0-34e5dbc.tar.gz"
-    sha256 "b62c27dee8599fb2756f75463aa08f6b2d1bbb5346db7df06c9f5785b8ae2df0"
+    url "https://github.com/TileDB-Inc/TileDB/releases/download/2.11.3/tiledb-macos-x86_64-2.11.3-a55a910.tar.gz"
+    sha256 "8eb10a7ba0858a666561a9d546f35b36b6792ed6f1d74812d74a39ec0c6dd8e0"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/TileDB-Inc/TileDB/releases/download/2.11.0/tiledb-linux-x86_64-2.11.0-34e5dbc.tar.gz"
-    sha256 "f4179de2d6df1eea658e0f9f68c4030b50ba85c0fcd878da8590a8bef0d1c1f3"
+    url "https://github.com/TileDB-Inc/TileDB/releases/download/2.11.3/tiledb-linux-x86_64-2.11.3-a55a910.tar.gz"
+    sha256 "84f5cfdab9b105c29dba74486ddd632a95cc975a884857f5615436c991a490e6"
   end
 
   def install
