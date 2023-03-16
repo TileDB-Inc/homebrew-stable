57c2d39c6683397abfe2c6baf606c92fb477d47f
Isaiah Norton <ihnorton@users.noreply.github.com>
2023-03-16 11:59:56 -0400

    Bump to TileDB 2.15 and add final release notice (#59)
    * Update to TileDB 2.15.0
    
    * Add deprecation notice

--- tiledb.rb
+++ tiledb.rb
@@ -1,23 +1,23 @@
 class Tiledb < Formula
   desc "Storage management library for sparse and dense array data"
   homepage "https://tiledb.com"
-  url "https://github.com/TileDB-Inc/TileDB/archive/refs/tags/2.14.0.tar.gz"
-  sha256 "e161d7988fa6e4e2e6238e25cbc0941bb2ed5eb375257d96ad7ce518864cd7f4"
+  url "https://github.com/TileDB-Inc/TileDB/archive/refs/tags/2.15.0.tar.gz"
+  sha256 "99d89f6fece0f14904b6b215721d3da0d7ab2d98145793d9256a0a1edf01c12d"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/TileDB-Inc/TileDB/releases/download/2.14.0/tiledb-macos-arm64-2.14.0-27eed08.tar.gz"
-    sha256 "9fcd22079e2b7f3af07a97a05f3a5463811d28d352be157947aae1ac13752739"
+    url "https://github.com/TileDB-Inc/TileDB/releases/download/2.15.0/tiledb-macos-arm64-2.15.0-1fb59c4.tar.gz"
+    sha256 "bc1f2928b21d910cd99374d9ba10e645488ecb2203be23edf87cd584007576ab"
   end
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/TileDB-Inc/TileDB/releases/download/2.14.0/tiledb-macos-x86_64-2.14.0-27eed08.tar.gz"
-    sha256 "ecdbfe58c89d06a63cb87c3e730cfd28dd18e52e7b634b45809603ae3bc79353"
+    url "https://github.com/TileDB-Inc/TileDB/releases/download/2.15.0/tiledb-macos-x86_64-2.15.0-1fb59c4.tar.gz"
+    sha256 "684eb5b3cc9cbcb0c167e906b91fee761a93c1d6e0ebe21c47cfbf6e13037e48"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/TileDB-Inc/TileDB/releases/download/2.14.0/tiledb-linux-x86_64-2.14.0-27eed08.tar.gz"
-    sha256 "402b20917b9d8b072d506e399f909444f8c45a40307493ea09eadfddb1f4bedc"
+    url "https://github.com/TileDB-Inc/TileDB/releases/download/2.15.0/tiledb-linux-x86_64-2.15.0-1fb59c4.tar.gz"
+    sha256 "62132d37157263f16fca9e29280dc66a497352481cf00d012874cadcb389d1ee"
   end
 
   def install
